f2d7e556f444cee8c9f7c8566d2a4045ca63f93e
Rajkumar <username@mydomain.com>
2012-04-01 11:45:55 -0500

    First draft for jqueryVersion example, tests pass

---
+++ examples/jqueryVersion/jquery_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"io/ioutil"
+	"log"
+	"testing"
+	"strings"
+)
+
+func equal(u,v []string) bool {
+	if len(u) != len(v) {
+		return false
+	}
+	for i,_ := range u {
+		if u[i] != v[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func readFile(fname string, t *testing.T) string {
+	b,err := ioutil.ReadFile(fname)
+	if err != nil {
+		t.Fatal("readFile",err)
+	}
+	return string(b)
+}
+
+func TestDefectiveScriptParser(t *testing.T) {
+	if l := len(findScriptSrc(`<!DOCTYPE HTML>
+    <html>
+    <body>
+
+    <video width="320" height="240" controls="controls">
+      <source src="movie.mp4" type="video/mp4" />
+	<source src="movie.ogg" type="video/ogg" />
+	  <source src="movie.webm" type="video/webm" />
+	  Your browser does not support the video tag.
+	  </video>
+
+	  </body>
+	  </html>`)); l != 0 {
+		t.Fail()
+	}
+	urls := findScriptSrc(readFile("w3schools.html",t))
+	if !equal(urls,[]string{"http://partner.googleadservices.com/gampad/google_service.js",
+			"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"}) {
+		t.Error("w3schools.html","src scripts are not recognized",urls)
+	}
+	urls = findScriptSrc(readFile("jquery_homepage.html",t))
+	if !equal(urls,[]string{"http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",
+			"http://code.jquery.com/jquery-1.4.2.min.js",
+			"http://static.jquery.com/files/rocker/scripts/custom.js",
+			"http://static.jquery.com/donate/donate.js"}) {
+		t.Error("jquery_homepage.html","src scripts are not recognized",urls)
+	}
+}
+
+func get(url string, t *testing.T) {
+}
+func proxyWithLog() (*http.Client,*bytes.Buffer) {
+	proxy := NewJqueryVersionProxy()
+	proxyServer := httptest.NewServer(proxy)
+	buf := new(bytes.Buffer)
+	proxy.Logger = log.New(buf,"",0)
+	proxyUrl, _ := url.Parse(proxyServer.URL)
+	tr := &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
+	client := &http.Client{Transport: tr}
+	return client,buf
+}
+func TestProxyServiceTwoVersions(t *testing.T) {
+	var fs    = httptest.NewServer(http.FileServer(http.Dir(".")))
+	defer fs.Close()
+
+	client,buf := proxyWithLog()
+
+	get := func(url string) {
+		resp, err := client.Get(fs.URL+url)
+		if err != nil {
+			t.Fatal("Cannot get proxy",err)
+		}
+		ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+	}
+
+	get("/w3schools.html")
+	get("/php_man.html")
+	if buf.String() != "" {
+		t.Error("shouldn't warn on a single URL",buf.String())
+	}
+	get("/jquery1.html")
+	warnings := buf.String()
+	if ! strings.Contains(warnings,"http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js") ||
+		! strings.Contains(warnings,"jquery.1.4.js") ||
+		! strings.Contains(warnings,"Contradicting") {
+		t.Error("contadicting jquery versions (php_man.html, w3schools.html) does not issue warning",warnings)
+	}
+}
+func TestProxyService(t *testing.T) {
+	var fs    = httptest.NewServer(http.FileServer(http.Dir(".")))
+	defer fs.Close()
+
+	client,buf := proxyWithLog()
+
+	get := func(url string) {
+		resp, err := client.Get(fs.URL+url)
+		if err != nil {
+			t.Fatal("Cannot get proxy",err)
+		}
+		ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+	}
+	get("/jquery_homepage.html")
+
+	warnings := buf.String()
+	if ! strings.Contains(warnings,"http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js") ||
+		! strings.Contains(warnings,"http://code.jquery.com/jquery-1.4.2.min.js") ||
+		! strings.Contains(warnings,"Contradicting") {
+		t.Error("contadicting jquery versions does not issue warning")
+	}
+
+}
---
+++ examples/jqueryVersion/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/elazarl/goproxy"
+	"github.com/elazarl/goproxy/ext/html"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func findScriptSrc(html string) []string {
+	// who said we can't parse HTML with regexp?
+	scriptMatcher := regexp.MustCompile(`(?i:<script\s+)`)
+	srcAttrMatcher := regexp.MustCompile(`^(?i:[^>]*\ssrc=["']([^"']*)["'])`)
+
+	srcs := make([]string,0)
+	matches := scriptMatcher.FindAllStringIndex(html,-1)
+	for _,match := range matches {
+		//println("Match",html[match[0]:match[1]])
+		// -1 to capture the whitespace at the end of the script tag
+		srcMatch := srcAttrMatcher.FindStringSubmatch(html[match[1]-1:])
+		if srcMatch != nil {
+			srcs = append(srcs,srcMatch[1])
+		}
+	}
+	return srcs
+}
+
+func NewJqueryVersionProxy() *goproxy.ProxyHttpServer {
+	proxy := goproxy.NewProxyHttpServer()
+	m := make(map[string]string)
+	jqueryMatcher := regexp.MustCompile(`(?i:jquery\.)`)
+	proxy.OnResponse(goproxy_html.IsHtml).Do(goproxy_html.HandleString(
+		func(s string, ctx *goproxy.ProxyCtx) string {
+			//ctx.Warnf("Charset %v by %v",ctx.Charset(),ctx.Req.Header["Content-Type"])
+			for _,src := range findScriptSrc(s) {
+				if jqueryMatcher.MatchString(src) {
+					prev,ok := m[ctx.Req.Host]
+					if ok && prev != src {
+						ctx.Warnf("In %v, Contradicting jqueries %v %v",ctx.Req.URL,prev,src)
+						break
+					}
+					m[ctx.Req.Host] = src
+				}
+			}
+			return s
+		}))
+	return proxy
+}
+
+func main() {
+	proxy := NewJqueryVersionProxy()
+	//proxy.Verbose = true
+	log.Fatal(http.ListenAndServe(":8080", proxy))
+}
