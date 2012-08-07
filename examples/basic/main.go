539dfb5be48de385ea9399e0e649fff24ab7bba2
Elazar Leibovich <elazarl@gmail.com>
2012-08-07 11:48:54 +0300

    log in basic example is off by default
    closes #1

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/elazarl/goproxy"
 	"log"
+	"flag"
 	"net/http"
 )
 
 func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
+	proxy.Verbose = *verbose
 	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
