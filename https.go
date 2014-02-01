2910762eeffb0167349144d461a03f9ad1c4748d
Leibovich, Elazar <elazarl@gmail.com>
2014-02-01 23:44:05 +0200

    fixes #34, react on dial errors

--- https.go
+++ https.go
@@ -88,9 +88,20 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 		} else {
 			targetSiteCon, e = proxy.dial("tcp", host)
 		}
-		if e != nil {
+		if e, ok := e.(net.Error); ok && e.Timeout() {
 			// trying to mimic the behaviour of the offending website
 			// don't answer at all
+			ctx.Warnf("Timeout dialing to %s: %s", host, e.Error())
+			return
+		}
+		if e != nil {
+			if _, err := io.WriteString(proxyClient, "HTTP/1.1 502 Bad Gateway\r\n\r\n"); err != nil {
+				ctx.Warnf("Error responding to client: %s", err)
+			}
+			ctx.Warnf("Error dialing to %s: %s", host, e.Error())
+			if err := proxyClient.Close(); err != nil {
+				ctx.Warnf("Error closing client connection: %s", err)
+			}
 			return
 		}
 		if https_proxy != "" {
