a0de2f9798311904ed85f8b6d0083fb60de4b8cd
Taliesin Sisson <taliesins@yahoo.com>
2015-07-06 23:55:21 +0100

    Add ruby gems Add node packages Add general packages

--- examples/goproxy-cache/main.go
+++ examples/goproxy-cache/main.go
@@ -21,10 +21,20 @@ var (
 	debug   bool
 )
 
-var matchUbuntuAptPattern = regexp.MustCompile(`(security|archive).ubuntu.com/ubuntu/(.+)$`)
+var matchUbuntuMirrorPattern = regexp.MustCompile(`(security|archive).ubuntu.com/ubuntu/(.+)$`)
+
 var matchAptPackagePattern = regexp.MustCompile(`(deb|udeb)$`)
 var matchAptPackageIndexPattern = regexp.MustCompile(`(DiffIndex|PackagesIndex|Packages\.(bz2|gz|lzma)|SourcesIndex|Sources\.(bz2|gz|lzma)|Release(\.gpg)?|Translation-(en|fr)\.(gz|bz2|bzip2|lzma))$`)
-var matchWindowsUpdatePackageIndexPattern = regexp.MustCompile(`(microsoft|windowsupdate|windows).com/.*\.(cab|exe|ms[i|u|f]|[ap]sf|wm[v|a]|dat|zip)`)
+
+var matchRubyGemPackageIndexPattern = regexp.MustCompile(`^rubygems.org/api/v1/dependencies|^rubygems.org/(prerelease_|latest_)?specs.*\.gz$`)
+var matchRubyGemPackagePattern = regexp.MustCompile(`rubygems.org/quick.*gemspec\.rz$|^rubygems.org/gems/.*\.gem$`)
+
+var matchNodePackagePattern = regexp.MustCompile(`^registry.npmjs.org/.*(\.tgz)`)
+var matchNodePackageIndexPattern = regexp.MustCompile(`registry.npmjs.org/.*$`)
+
+var matchWindowsUpdatePackagePattern = regexp.MustCompile(`(microsoft|windowsupdate|windows).com/.*\.(cab|exe|ms[i|u|f]|[ap]sf|wm[v|a]|dat|zip)`)
+
+var commonPackagePattern = regexp.MustCompile(`/.*\.(zip|exe|msi|7z|rar|tgz|iso|cab|jar|gz|bz2|bzip2|lzma)`)
 
 func init() {
 	flag.StringVar(&listen, "listen", defaultListen, "the host and port to bind to")
@@ -52,12 +62,17 @@ func main() {
 
 	proxy := goproxy.NewProxyHttpServer()
 
-	matchUbuntuApt := goproxy.UrlMatches(matchUbuntuAptPattern)
+	matchUbuntuMirror := goproxy.UrlMatches(matchUbuntuMirrorPattern)
 	matchAptPackage := goproxy.UrlMatches(matchAptPackagePattern)
 	matchAptPackageIndex := goproxy.UrlMatches(matchAptPackageIndexPattern)
-	matchWindowsUpdatePackageIndex := goproxy.UrlMatches(matchWindowsUpdatePackageIndexPattern)
+	matchRubyGemPackageIndex := goproxy.UrlMatches(matchRubyGemPackageIndexPattern)
+	matchRubyGemPackage := goproxy.UrlMatches(matchRubyGemPackagePattern)
+	matchNodePackage := goproxy.UrlMatches(matchNodePackagePattern)
+	matchNodePackageIndex := goproxy.UrlMatches(matchNodePackageIndexPattern)
+	matchWindowsUpdatePackage := goproxy.UrlMatches(matchWindowsUpdatePackagePattern)
+	commonPackage := goproxy.UrlMatches(commonPackagePattern)
 
-	proxy.OnRequest(matchUbuntuApt).DoFunc(
+	proxy.OnRequest(matchUbuntuMirror).DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 			url := r.URL.String()
 			ubuntuRewriter.Rewrite(r)
@@ -86,12 +101,62 @@ func main() {
 			return TryCacheResponse(true, cache, r)
 		})
 
-	proxy.OnRequest(matchWindowsUpdatePackageIndex).DoFunc(
+	proxy.OnRequest(matchRubyGemPackage).DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 			return TryServeCachedResponse(true, cache, r)
 		})
 
-	proxy.OnResponse(matchWindowsUpdatePackageIndex).DoFunc(
+	proxy.OnResponse(matchRubyGemPackage).DoFunc(
+		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			return TryCacheResponse(true, cache, r)
+		})
+
+	proxy.OnRequest(matchRubyGemPackageIndex).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			return TryServeCachedResponse(true, cache, r)
+		})
+
+	proxy.OnResponse(matchRubyGemPackageIndex).DoFunc(
+		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			return TryCacheResponse(true, cache, r)
+		})
+
+	proxy.OnRequest(matchNodePackage).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			return TryServeCachedResponse(true, cache, r)
+		})
+
+	proxy.OnResponse(matchNodePackage).DoFunc(
+		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			return TryCacheResponse(true, cache, r)
+		})
+
+	proxy.OnRequest(matchNodePackageIndex).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			return TryServeCachedResponse(true, cache, r)
+		})
+
+	proxy.OnResponse(matchNodePackageIndex).DoFunc(
+		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			return TryCacheResponse(true, cache, r)
+		})
+
+	proxy.OnRequest(matchWindowsUpdatePackage).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			return TryServeCachedResponse(true, cache, r)
+		})
+
+	proxy.OnResponse(matchWindowsUpdatePackage).DoFunc(
+		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			return TryCacheResponse(true, cache, r)
+		})
+
+	proxy.OnRequest(commonPackage).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			return TryServeCachedResponse(true, cache, r)
+		})
+
+	proxy.OnResponse(commonPackage).DoFunc(
 		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 			return TryCacheResponse(true, cache, r)
 		})
