60daedee06225ad11a692db85de7a7345d147421
Benji Hooper <benji@sixp.in>
2017-02-22 17:28:32 +0000

    Making goproxy go1.8 ready

--- ext/auth/basic.go
+++ ext/auth/basic.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/elazarl/goproxy"
+	"github.com/SpectoLabs/goproxy"
 )
 
 var unauthorizedMsg = []byte("407 Proxy Authentication Required")
--- ext/auth/basic_test.go
+++ ext/auth/basic_test.go
@@ -14,8 +14,8 @@ import (
 	"sync/atomic"
 	"testing"
 
-	"github.com/elazarl/goproxy"
-	"github.com/elazarl/goproxy/ext/auth"
+	"github.com/SpectoLabs/goproxy"
+	"github.com/SpectoLabs/goproxy/ext/auth"
 )
 
 type ConstantHanlder string
