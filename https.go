0f7600078240b0119f6fdaa5011653fc0cc40868
Tommy Situ <tommy.situ@specto.io>
2018-01-02 17:38:14 +0000

    Only dial connection to remote server after proxy filtered requests for ConnectHttpMitm

--- https.go
+++ https.go
@@ -127,14 +127,8 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 	case ConnectHTTPMitm:
 		proxyClient.Write([]byte("HTTP/1.0 200 OK\r\n\r\n"))
 		ctx.Logf("Assuming CONNECT is plain HTTP tunneling, mitm proxying it")
-		targetSiteCon, err := proxy.connectDial("tcp", host)
-		if err != nil {
-			ctx.Warnf("Error dialing to %s: %s", host, err.Error())
-			return
-		}
 		for {
 			client := bufio.NewReader(proxyClient)
-			remote := bufio.NewReader(targetSiteCon)
 			req, err := http.ReadRequest(client)
 			if err != nil && err != io.EOF {
 				ctx.Warnf("cannot read request of MITM HTTP client: %+#v", err)
@@ -144,6 +138,13 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 			}
 			req, resp := proxy.filterRequest(req, ctx)
 			if resp == nil {
+				// only connect to remote server if proxy does not produce a response
+				targetSiteCon, err := proxy.connectDial("tcp", host)
+				if err != nil {
+					ctx.Warnf("Error dialing to %s: %s", host, err.Error())
+					return
+				}
+				remote := bufio.NewReader(targetSiteCon)
 				if err := req.Write(targetSiteCon); err != nil {
 					httpError(proxyClient, ctx, err)
 					return
