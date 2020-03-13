dfa30ed7a545498a46478e7db4c0d9d0911a607b
alex <alex@windscribe.com>
2020-03-13 16:35:27 -0400

    tproxy support

--- https.go
+++ https.go
@@ -139,6 +139,8 @@ func (proxy *ProxyHttpServer) handleHttpsConnectAccept(ctx *ProxyCtx, host strin
 
 	} else if ctx.ForwardProxyTProxy {
 
+		ctx.Logf("dialing from: %s", ctx.ForwardProxySourceIP)
+
 		tcpLocal, errTCP := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:0", ctx.ForwardProxySourceIP))
 		if errTCP != nil {
 			ctx.Logf("Failed to resolve local TCP address: %s - err: %v", ctx.ForwardProxySourceIP, err)
