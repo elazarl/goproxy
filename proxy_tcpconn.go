fe7d82b20bd547654c1fbd76b9c4cb1cc0f146ce
Alex <alex@Alexs-MacBook-Pro.local>
2020-05-18 19:20:09 -0400

    proxyTCPConn

--- proxy_tcpconn.go
+++ proxy_tcpconn.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"net/http"
 	"reflect"
-	"syscall"
 	"time"
 
 	"github.com/function61/gokit/logex"
@@ -66,31 +65,31 @@ func (conn *proxyTCPConn) setKeepaliveParameters(count, interval, period int) er
 	if setErr != nil {
 		return setErr
 	}
-	rawConn, err := tcpConn.SyscallConn()
-	if err != nil {
-		return err
-	}
-	err = rawConn.Control(
-		func(fdPtr uintptr) {
-			// got socket file descriptor. Setting parameters.
-			fd := int(fdPtr)
+	// rawConn, err := tcpConn.SyscallConn()
+	// if err != nil {
+	// 	return err
+	// }
+	// err = rawConn.Control(
+	// 	func(fdPtr uintptr) {
+	// 		// got socket file descriptor. Setting parameters.
+	// 		fd := int(fdPtr)
 
-			conn.Logger.Info.Printf("attempting KA options on: %+v - fd: %+v", rawConn, fd)
+	// 		conn.Logger.Info.Printf("attempting KA options on: %+v - fd: %+v", rawConn, fd)
 
-			//Number of probes.
-			err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.TCP_KEEPCNT, count)
-			if err != nil {
-				conn.Logger.Error.Printf("on setting keepalive probe count: %s", err.Error())
-			}
-			//Wait time after an unsuccessful probe.
-			err = syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.TCP_KEEPINTVL, interval)
-			if err != nil {
-				conn.Logger.Error.Printf("on setting keepalive retry interval: %s", err.Error())
-			}
-		})
-	if err != nil {
-		return err
-	}
+	// 		//Number of probes.
+	// 		err := syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_KEEPCNT, count)
+	// 		if err != nil {
+	// 			conn.Logger.Error.Printf("on setting keepalive probe count: %s", err.Error())
+	// 		}
+	// 		//Wait time after an unsuccessful probe.
+	// 		err = syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_KEEPINTVL, interval)
+	// 		if err != nil {
+	// 			conn.Logger.Error.Printf("on setting keepalive retry interval: %s", err.Error())
+	// 		}
+	// 	})
+	// if err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
