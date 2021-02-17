b067ff15d804ef72a49ebfd08de578379fb63797
Michael Gartsbein <3885814+mikegarts@users.noreply.github.com>
2021-02-17 18:16:37 +0200

    support upstream proxy in simple example

--- examples/goproxy-basic/main.go
+++ examples/goproxy-basic/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"flag"
 	"github.com/elazarl/goproxy"
 	"log"
-	"flag"
 	"net/http"
+	"net/url"
 )
 
 func main() {
 	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
 	addr := flag.String("addr", ":8080", "proxy listen address")
+	upstream := flag.String("upstream", "", "upstream proxy if relevant")
 	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = *verbose
+
+	log.Printf("Listening on %s", *addr)
+
+	if (*upstream != "") {
+		log.Printf("Setting upstream proxy to %s", *upstream)
+		proxy.Tr = &http.Transport{Proxy: func(req *http.Request) (*url.URL, error) {
+			return url.Parse(*upstream)
+		}}
+		proxy.ConnectDial = proxy.NewConnectDialToProxy(*upstream)
+
+	}
 	log.Fatal(http.ListenAndServe(*addr, proxy))
+
 }
