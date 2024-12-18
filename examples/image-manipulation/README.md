05102164e712611964b62a2fc10c3ebf48369285
Erik Pellizzon <erikpellizzon1@gmail.com>
2024-12-18 14:52:48 +0100

    Refactor image example

---
+++ examples/image-manipulation/README.md
@@ -0,0 +1,4 @@
+# Image Manipulation
+
+This example starts a proxy server that manipulate the received images,
+to make them appear upside down.
--- examples/image-manipulation/main.go
+++ examples/image-manipulation/main.go
@@ -13,13 +13,13 @@ func main() {
 	proxy.OnResponse().Do(goproxy_image.HandleImage(func(img image.Image, ctx *goproxy.ProxyCtx) image.Image {
 		dx, dy := img.Bounds().Dx(), img.Bounds().Dy()
 
-		nimg := image.NewRGBA(img.Bounds())
+		newImg := image.NewRGBA(img.Bounds())
 		for i := 0; i < dx; i++ {
 			for j := 0; j <= dy; j++ {
-				nimg.Set(i, j, img.At(i, dy-j-1))
+				newImg.Set(i, j, img.At(i, dy-j-1))
 			}
 		}
-		return nimg
+		return newImg
 	}))
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
